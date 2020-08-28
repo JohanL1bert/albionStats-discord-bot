306bbe34814868e0137af0fb416706f9053f0fa4
Johan <grimmerw968@gmail.com>
2020-08-28 19:56:36 +0300

    minore fixes

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ TOKEN, ID_CHANNEL, CHANNEL_NAME = env_check()
 
 
 #checkers server room
-#TODO: Возможно переписать под множества каналов. Удалить else
+#FIXME: Возможно переписать под множества каналов. Удалить else. Дергать только существующие каналы 
 @client_bot.event
 async def on_message(ctx):
     if ctx.channel.id == ID_CHANNEL:
@@ -43,7 +43,8 @@ async def on_message(ctx):
 #FIXME: Переписать проверку. Возможно лучше по каналам сделать
 @client_bot.event
 async def on_connect():
-    await client_bot.get_channel(ID_CHANNEL).send("Bot is connected to channel")
+    start_message = ("```Bot is connected to channel```")
+    await client_bot.get_channel(ID_CHANNEL).send(start_message)
 
 
 
@@ -57,20 +58,22 @@ async def help(ctx):
         description = "Description of the commands"
     )
 
-    embed.add_field(name = "add_player", value = "This will add user", inline = False)
-    embed.add_field(name = "add_guild name", value = "Add guild to track", inline = False)
-    embed.add_field(name = "status_bot", value = "Check bot status", inline = False)
-    embed.add_field(name = "status_API", value = "Check is albion site is not down", inline = False)
+    embed.add_field(name = "add player", value = "This will add user", inline = False)
+    embed.add_field(name = "add guild name", value = "Add guild to track", inline = False)
+    embed.add_field(name = "remove watch player", value = "Remove track from player", inline= False)
+    embed.add_field(name = "remove watch guild", value = "Remove track from guild", inline= False)
+    embed.add_field(name = "status bot", value = "Check bot status", inline = False)
+    embed.add_field(name = "status API", value = "Check is albion site is not down", inline = False)
 
-
-    await client_bot.get_channel(ID_CHANNEL).send(embed = embed)
+    await client_bot.get_channel(ID_CHANNEL).send(embed = embed) # ctx.send(embed = embed)
 
 
 #Commands
 #TODO: Больше команд. Исправить название. Написать функции
 @client_bot.command()
-async def add_player():
-    pass
+async def add_player(ctx, name):
+    await ctx.send("Player name - {} is add".format(name))
+
 
 @client_bot.command()
 async def add_guild():
@@ -86,7 +89,13 @@ async def albion_status():
 
 
 #Save 
+#TODO: Возможно лучше сделать один файл для всех сейвов
+def save_player_name():
+    pass
 
+
+
+#requestest 
 
 client_bot.run(TOKEN)
 
@@ -100,5 +109,6 @@ KB_ALBION_URL = "https://albiononline.com/en/killboard/battles/"
 # API ALBION
 # embed доделать
 # можно сделать bot_prefix customize
+
 
 
